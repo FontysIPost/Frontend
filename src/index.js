64ab2cb9ebb6552d5512ccfa0ab6f404cbe3bf42
Giang <463189@student.fontys.nl>
2023-07-05 17:43:45 +0200

    feat: Initial separation frontend from backend

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ImageBarcodeReader from "./components/BarCodeScanner/ImageBarcodeReader.vue";
+import StreamBarcodeReader from "./components/BarCodeScanner/StreamBarcodeReader.vue";
+import Signature from "./components/BarCodeScanner/Signature.vue";
+export { ImageBarcodeReader, StreamBarcodeReader, Signature };
